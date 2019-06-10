e3225775dadd3a9b8e3f999f063388bd4fd52af2
akolybelnikov <a.kolybelnikov@gmail.com>
2019-06-10 19:08:08 +0200

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -2,6 +2,7 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.getRegistrations().then(registrations => {
     if (registrations.length) {
       for (let registration of registrations) {
+        console.log(registration)
         registration.unregister()
       }
     }
