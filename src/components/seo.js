333b0fd7e24a412e27e018cea5c34a0f76d310de
akolybelnikov <a.kolybelnikov@gmail.com>
2020-03-25 18:22:24 +0100

    Added facebook pixel tag.

--- src/components/seo.js
+++ src/components/seo.js
@@ -50,6 +50,29 @@ function SEO({ description, lang, meta, title }) {
     >
       <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
       <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+      <script>
+        {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '549514382358073'); 
+            fbq('track', 'PageView');
+        `}
+      </script>
+      <noscript>
+        {`
+            <img
+            alt="facebook noscript page view"
+            height="1"
+            width="1"
+            src="https://www.facebook.com/tr?id=549514382358073&ev=PageView&noscript=1" />
+        `}
+      </noscript>
     </Helmet>
   )
 }
