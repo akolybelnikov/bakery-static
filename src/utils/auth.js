f4a984be2ef88af6b1fbdd7d8c88e5b8c0e91e6f
akolybelnikov <a.kolybelnikov@gmail.com>
2020-09-13 18:55:35 +0200

    Fixed build.

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,21 +1,29 @@
-import { window } from "browser-monads"
+const isBrowser = typeof window !== `undefined`
 
 export const setUser = user =>
-  (window.localStorage.COGNITO_USER = JSON.stringify(user))
+  isBrowser && (window.localStorage.COGNITO_USER = JSON.stringify(user))
 
-const getUser = () =>
-  window.localStorage.COGNITO_USER
-    ? JSON.parse(window.localStorage.COGNITO_USER)
-    : {}
+const getUser = () => {
+  if (!isBrowser) return
+
+  if (window.localStorage.COGNITO_USER) {
+    let user = JSON.parse(window.localStorage.COGNITO_USER)
+    return user ? user : {}
+  }
+  return {}
+}
 
 export const isLoggedIn = () => {
+  if (!isBrowser) return false
+
   const user = getUser()
   if (user) return !!user.username
 }
 
-export const getCurrentUser = () => getUser()
+export const getCurrentUser = () => isBrowser && getUser()
 
 export const logout = callback => {
+  if (!isBrowser) return
   setUser({})
   callback()
 }
