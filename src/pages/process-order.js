e4d44b4e55f3890a43d6b79bab17e207e5eee6ef
akolybelnikov <a.kolybelnikov@gmail.com>
2020-09-25 15:01:38 +0200

    Send DB request before the action dispatch.

--- src/pages/process-order.js
+++ src/pages/process-order.js
@@ -21,8 +21,6 @@ import { useUserDispatch, useUserState } from "../state/user"
 import { getCurrentUser } from "../utils/auth"
 import { PAGE, USER } from "../utils/utils"
 
-axios.defaults.headers['Referer'] = "https://vsebulochki.com/process-order"
-
 const useStyles = makeStyles({
   root: {
     marginBlockEnd: "2rem",
@@ -239,12 +237,12 @@ const ProcessOrder = ({ location }) => {
               : "Ваш заказ принят. Для того, чтобы уточнить условия доставки, с Вами в ближайшее время свяжется наш менеджер.",
         },
       })
-      // empty the cart
-      dispatch({ type: "EMPTY_CART" })
       // save the order to the database
       if (currentPage === PAGE.PICK_UP) {
         await safe(createOrder(order))
       }
+      // empty the cart
+      dispatch({ type: "EMPTY_CART" })
     }
   }
 
